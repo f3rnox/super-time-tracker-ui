224af0a3d3531742c4502a0683919711fa7ebebd
Cris Mihalache <f3rnox42@gmail.com>
2026-05-19 21:38:30 +0300

    feat: reduce build size and fix built styles

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'standalone',
   distDir: 'next-output',
+  images: {
+    unoptimized: true,
+  },
+  outputFileTracingExcludes: {
+    '/*': [
+      'node_modules/sharp/**',
+      'node_modules/@img/**',
+      'node_modules/next/dist/compiled/next-server/*-experimental.runtime.prod.js',
+    ],
+  },
 }
 
-export default nextConfig;
+export default nextConfig
